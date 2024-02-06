ca078ecbed2267d81f4f1d768e19e575c2912d25
Evelina <138245019+Eva8210@users.noreply.github.com>
2024-02-06 10:39:18 +0200

    Add files via upload

---
+++ ArrayList_AL2 (done)/AL2_10_TheGreatest/src/TheGreatest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class TheGreatest {
+    public static int greatest(ArrayList<Integer> list) {
+        int greatest = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) > greatest) {
+                greatest = list.get(i);
+            }
+        }
+        return greatest;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+        
+        System.out.println("The greatest number is: " + greatest(list));
+    }
+}
